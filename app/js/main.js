3786445641e89b668345623fea6b32887efbd2cc
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2025-03-09 13:17:05 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,46 @@
+import {setupMobileMenu} from "./mobileMenu.js";
+
+setupMobileMenu();
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const targetId = event.target.getAttribute('href').slice(1);
+  const targetElement = document.getElementById(targetId);
+
+  let targetOffset;
+
+  targetOffset = targetElement.offsetTop - 10;
+  window.scrollTo({top: targetOffset, behavior: 'smooth'});
+}
+
+const menuLinks = document.querySelectorAll('.desktop');
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', scrollToSection);
+});
+
+new Swiper('.services__swiper', {
+  navigation: {
+    nextEl: '.services__btn--next',
+    prevEl: '.services__btn--prev',
+  },
+  breakpoints: {
+    700: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    }
+  }
+});
+
+new Swiper('.reviews__swiper', {
+  navigation: {
+    nextEl: '.reviews__btn--next',
+    prevEl: '.reviews__btn--prev',
+  },
+  breakpoints: {
+    700: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    }
+  }
+});
